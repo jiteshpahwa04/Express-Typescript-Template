94166c8be63dcc5f03f16bf2eaec84e9ec08b599
Jitesh Pahwa <jiteshofficial2004@gmail.com>
2025-05-04 21:43:44 +0530

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { Express } from "express";
+
+const app: Express = express();
+
+const PORT = 3000; // implicit type-def
+
+app.get('/ping', (req, res)=>{
+    res.send("Pong");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port: ${PORT}`);
+});
