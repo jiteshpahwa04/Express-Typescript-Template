6f74de8c7c0c879a40e8cdedf6a602832283b55e
Jitesh Pahwa <jiteshofficial2004@gmail.com>
2025-05-08 01:04:47 +0530

    Added error handling

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { Express } from "express";
 import {serverConfig} from "./config";
 import v1Router from "./routers/v1/index.router";
 import v2Router from "./routers/v2/index.router";
+import { genericErrorHandler } from "./middlewares/error.middleware";
 
 const app: Express = express();
 
@@ -13,6 +14,9 @@ app.use(express.json());
 // Registering all the routers and their corresponding routes with our app server object
 app.use('/api/v1', v1Router);
 app.use('/api/v2', v2Router);
+
+// Error handler middleware
+app.use(genericErrorHandler);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port: ${PORT}`);
