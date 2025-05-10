7a92f49961b2e899e6bba476e6b34a49205d16b8
Jitesh Pahwa <jiteshofficial2004@gmail.com>
2025-05-11 00:57:51 +0530

    Added loggers

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import {serverConfig} from "./config";
 import v1Router from "./routers/v1/index.router";
 import v2Router from "./routers/v2/index.router";
 import { genericErrorHandler } from "./middlewares/error.middleware";
+import logger from "./config/logger.config";
+import { attachCorrelationMiddleWare } from "./middlewares/correlation.middleware";
 
 const app: Express = express();
 
@@ -11,6 +13,7 @@ const PORT = serverConfig.PORT;
 
 app.use(express.json());
 
+app.use(attachCorrelationMiddleWare);
 // Registering all the routers and their corresponding routes with our app server object
 app.use('/api/v1', v1Router);
 app.use('/api/v2', v2Router);
@@ -19,5 +22,5 @@ app.use('/api/v2', v2Router);
 app.use(genericErrorHandler);
 
 app.listen(PORT, ()=>{
-    console.log(`Server is running on port: ${PORT}`);
+    logger.info(`Server is running on port: ${PORT}`);
 });
